a0baa28b5b4df0ee18a21ba3a3754afdb1fb9274
sandeep8093 <nayaksandeep676@gmail.com>
2024-02-03 13:38:47 +0530

    cors added

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 require('dotenv').config();
-const express= require('express')
+const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
 
 const PORT = 6500;
 
 app.use(express.raw());
 app.use(express.json());
 
-mongoose.connect(process.env.DB_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology : true
-}).then((data)=>{
-    console.log('MongoDB Connected');
-}).catch((err)=>{
-    console.log(err)
+app.use(cors());
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then((data) => {
+  console.log('MongoDB Connected');
+}).catch((err) => {
+  console.log(err);
 });
 
 app.get('/', function (req, res) {
-    res.send('hello world')
-})
+  res.send('hello world');
+});
 
-app.use('/api',require('./src/routes/route'))
+app.use('/api', require('./src/routes/route'));
 
-app.listen(PORT,()=>{
-    console.log(`Server Running on port: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server Running on port: ${PORT}`);
+});
